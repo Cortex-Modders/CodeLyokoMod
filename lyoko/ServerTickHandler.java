f571291a47bb25b216adba031269a75d8ecfe151
Matthew Warren <tonkamatt98@gmail.com>
2013-02-28 16:53:30 -0600

    Updated Armor
    moved special power code from ServerTickHandler.java to ArmorLyoko.java
    and fixed aelita's flying ability

--- lyoko/ServerTickHandler.java
+++ lyoko/ServerTickHandler.java
@@ -18,66 +18,7 @@ public class ServerTickHandler implements ITickHandler
 	public int lifePoints = 80;
 	
 	private void onPlayerTick(EntityPlayer player)
-	{
-		/*
-		if(player.inventory.armorInventory == new ItemStack[]{new ItemStack(CodeLyoko.AelitaBoots), new ItemStack(CodeLyoko.AelitaLegs), new ItemStack(CodeLyoko.AelitaChest), new ItemStack(CodeLyoko.AelitaHelmet)})
-		{
-			player.capabilities.allowFlying = true;
-		}
-		*/
-		if (player.getCurrentItemOrArmor(4) != null && player.getCurrentItemOrArmor(3) != null
-				&& player.getCurrentItemOrArmor(2) != null && player.getCurrentItemOrArmor(1) != null)
-		{    
-			ItemStack helmet = player.getCurrentItemOrArmor(4);
-			ItemStack chest = player.getCurrentItemOrArmor(3);
-			ItemStack legs = player.getCurrentItemOrArmor(2);
-			ItemStack boots = player.getCurrentItemOrArmor(1);
-			if (helmet.getItem() == CodeLyoko.AelitaHelmet && chest.getItem() == CodeLyoko.AelitaChest
-					&& legs.getItem() == CodeLyoko.AelitaLegs && boots.getItem() == CodeLyoko.AelitaBoots)
-			{
-				player.capabilities.allowFlying = true;
-				player.fallDistance = 0;
-				for(int x = 0; x < 9; x++)
-				{
-					ItemStack stack = player.inventory.getStackInSlot(x);
-					if((stack == null || stack == new ItemStack(CodeLyoko.VirtualBlock, stack.stackSize)) && !player.inventory.hasItem(CodeLyoko.VirtualBlock.blockID))
-					{
-						player.inventory.setInventorySlotContents(x, new ItemStack(CodeLyoko.VirtualBlock, 1));
-					}
-				}
-			}
-			else if (helmet.getItem() == CodeLyoko.OddHelmet && chest.getItem() == CodeLyoko.OddChest
-					&& legs.getItem() == CodeLyoko.OddLegs && boots.getItem() == CodeLyoko.OddBoots)
-			{
-				player.addPotionEffect((new PotionEffect(Potion.jump.getId(), 20, 3)));
-				player.fallDistance = 0;
-			}
-			else if (helmet.getItem() == CodeLyoko.UlrichHelmet && chest.getItem() == CodeLyoko.UlrichChest
-					&& legs.getItem() == CodeLyoko.UlrichLegs && boots.getItem() == CodeLyoko.UlrichBoots)
-			{
-				if(player.isSprinting())
-				{
-					player.addPotionEffect((new PotionEffect(Potion.moveSpeed.getId(), 20, 2)));
-				}
-				player.fallDistance = 0;
-			}
-			else if (helmet.getItem() == CodeLyoko.YumiHelmet && chest.getItem() == CodeLyoko.YumiChest
-					&& legs.getItem() == CodeLyoko.YumiLegs && boots.getItem() == CodeLyoko.YumiBoots)
-			{
-				player.fallDistance = 0;
-			}
-			else if (helmet.getItem() == CodeLyoko.WilliamHelmet && chest.getItem() == CodeLyoko.WilliamChest
-					&& legs.getItem() == CodeLyoko.WilliamLegs && boots.getItem() == CodeLyoko.WilliamBoots)
-			{
-				if(player.isSprinting())
-				{
-					player.addPotionEffect((new PotionEffect(Potion.moveSpeed.getId(), 20, 2)));
-					player.addPotionEffect((new PotionEffect(Potion.invisibility.getId(), 20, 2)));
-				}
-				player.fallDistance = 0;
-			}
-		}
-		
+	{	
 		if(player.username.equals("986523714") || player.username.equals("MoonMagick") || player.username.equals("Wolfspirit1st"))
 		{
 			player.playerCloakUrl = "https://dl.dropbox.com/u/87762025/lyokocape.png";
--- lyoko/items/ArmorLyoko.java
+++ lyoko/items/ArmorLyoko.java
@@ -2,6 +2,8 @@ package matt.lyoko.items;
 
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.*;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
 import net.minecraft.src.*;
 import net.minecraft.world.World;
 import net.minecraftforge.common.*;
@@ -25,6 +27,68 @@ public class ArmorLyoko extends ItemArmor implements IArmorTextureProvider
         public String getTextureFile()
         {
                 return "/matt/lyoko/gui/items.png";
+        }
+        
+        public void onArmorTickUpdate(World world, EntityPlayer player, ItemStack stack)
+        {
+        	if (player.getCurrentItemOrArmor(4) != null && player.getCurrentItemOrArmor(3) != null
+    				&& player.getCurrentItemOrArmor(2) != null && player.getCurrentItemOrArmor(1) != null)
+    		{    
+    			ItemStack helmet = player.getCurrentItemOrArmor(4);
+    			ItemStack chest = player.getCurrentItemOrArmor(3);
+    			ItemStack legs = player.getCurrentItemOrArmor(2);
+    			ItemStack boots = player.getCurrentItemOrArmor(1);
+    			if (helmet.getItem() == CodeLyoko.AelitaHelmet && chest.getItem() == CodeLyoko.AelitaChest
+    					&& legs.getItem() == CodeLyoko.AelitaLegs && boots.getItem() == CodeLyoko.AelitaBoots)
+    			{
+    				player.capabilities.allowFlying = true;
+    				player.fallDistance = 0;
+    				for(int x = 0; x < 9; x++)
+    				{
+    					ItemStack stack2 = player.inventory.getStackInSlot(x);
+    					if((stack2 == null || stack2 == new ItemStack(CodeLyoko.VirtualBlock, stack2.stackSize)) && !player.inventory.hasItem(CodeLyoko.VirtualBlock.blockID))
+    					{
+    						player.inventory.setInventorySlotContents(x, new ItemStack(CodeLyoko.VirtualBlock, 1));
+    					}
+    				}
+    			}
+    			else if (helmet.getItem() == CodeLyoko.OddHelmet && chest.getItem() == CodeLyoko.OddChest
+    					&& legs.getItem() == CodeLyoko.OddLegs && boots.getItem() == CodeLyoko.OddBoots)
+    			{
+    				player.addPotionEffect((new PotionEffect(Potion.jump.getId(), 20, 3)));
+    				player.fallDistance = 0;
+    			}
+    			else if (helmet.getItem() == CodeLyoko.UlrichHelmet && chest.getItem() == CodeLyoko.UlrichChest
+    					&& legs.getItem() == CodeLyoko.UlrichLegs && boots.getItem() == CodeLyoko.UlrichBoots)
+    			{
+    				if(player.isSprinting())
+    				{
+    					player.addPotionEffect((new PotionEffect(Potion.moveSpeed.getId(), 20, 2)));
+    				}
+    				player.fallDistance = 0;
+    			}
+    			else if (helmet.getItem() == CodeLyoko.YumiHelmet && chest.getItem() == CodeLyoko.YumiChest
+    					&& legs.getItem() == CodeLyoko.YumiLegs && boots.getItem() == CodeLyoko.YumiBoots)
+    			{
+    				player.fallDistance = 0;
+    			}
+    			else if (helmet.getItem() == CodeLyoko.WilliamHelmet && chest.getItem() == CodeLyoko.WilliamChest
+    					&& legs.getItem() == CodeLyoko.WilliamLegs && boots.getItem() == CodeLyoko.WilliamBoots)
+    			{
+    				if(player.isSprinting())
+    				{
+    					player.addPotionEffect((new PotionEffect(Potion.moveSpeed.getId(), 20, 2)));
+    					player.addPotionEffect((new PotionEffect(Potion.invisibility.getId(), 20, 2)));
+    				}
+    				player.fallDistance = 0;
+    			}
+    		}
+        	else if ((player.getCurrentItemOrArmor(4) == null || player.getCurrentItemOrArmor(3) == null
+    				|| player.getCurrentItemOrArmor(2) == null || player.getCurrentItemOrArmor(1) == null)
+    				&& player.capabilities.isCreativeMode == false)
+			{
+				player.capabilities.allowFlying = false;
+			}
         }
         
         public String getArmorTextureFile(ItemStack itemstack)
