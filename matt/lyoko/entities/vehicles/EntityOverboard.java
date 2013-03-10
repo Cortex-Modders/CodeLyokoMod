e113234a14498fa70b6812111af442f93008badd
Jake Rhoda <jake@jadarstudios.com>
2013-03-09 20:12:08 -0600

    added overboard. no texture yet tho, and collisions are screwed up.

--- matt/lyoko/entities/vehicles/EntityOverboard.java
+++ matt/lyoko/entities/vehicles/EntityOverboard.java
@@ -3,17 +3,51 @@ package matt.lyoko.entities.vehicles;
 import matt.lyoko.entities.mobs.EntityLyoko;
 import net.minecraft.world.World;
 
-public class EntityOverboard extends EntityLyoko
-{
-	public EntityOverboard(World world)
-	{
-		super(world);
-        texture = "/matt/lyoko/mob/overboard.png";
-	}
+public class EntityOverboard extends EntityLyoko {
 	
-	@Override
-	public int getMaxHealth()
-	{
-		return 200;
+	public EntityOverboard(World par1World) {
+		super(par1World);
+		texture = "/matt/lyoko/mob/overboard.png";
 	}
+
+	/**
+	 * Returns the maximum health of the entity.
+	 */
+	public int getMaxHealth() {
+		// TODO Auto-generated method stub
+		return 1000;
+	}
+
+	/**
+     * Returns the sound this mob makes while it's alive.
+     */
+    protected String getLivingSound()
+    {
+        return null;
+    }
+
+    /**
+     * Returns the sound this mob makes when it is hurt.
+     */
+    protected String getHurtSound()
+    {
+        return null;
+    }
+
+    /**
+     * Returns the sound this mob makes on death.
+     */
+    protected String getDeathSound()
+    {
+        return null;
+    }
+    
+    /**
+     * Returns true if this entity should push and be pushed by other entities when colliding.
+     */
+    public boolean canBePushed()
+    {
+        return true;
+    }
+	
 }
