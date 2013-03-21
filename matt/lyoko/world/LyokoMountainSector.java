36b1c89001995f55637d80fce654eae4209d2bc3
Jake Rhoda <jake@jadarstudios.com>
2013-03-21 18:15:22 -0500

    fixed crash from method func_80007_l(), as it is the same as getDimensionName(). also fixed spacing..

--- matt/lyoko/world/LyokoMountainSector.java
+++ matt/lyoko/world/LyokoMountainSector.java
@@ -11,95 +11,94 @@ import net.minecraft.world.chunk.IChunkProvider;
 import matt.lyoko.*;
 
 public class LyokoMountainSector extends WorldProvider {
-	
-	 public int getDimensionID() 
-	 {
-	  return CodeLyoko.Mountain_Sector_ID;
-	 }
-	 
-	 public void registerWorldChunkManager()
-	    {
-		    worldChunkMgr = new WorldChunkManagerHell(CodeLyoko.lyokomountain, 0.8F, 0F);
-	    }
-	 
-	    public IChunkProvider getChunkProvider()
-	    {
-	        return new LyokoMountainChunkProvider(worldObj, worldObj.getSeed());
-	    }
-	 
-	 public boolean canRespawnHere()
-	    {
-	        return false;
-	    }
-	 
-	 public String func_80007_l() 
-	 {
-	  return null;
-	 }
-	 
-	 //Ender sky if set true
-	 public boolean renderEndSky()
-	 {
-	  return true;
-	 }
-	 
-	 public float setSunSize()
-	    {
-	        return 2.0F;
-	    }
-	 
-	    public float setMoonSize()
-	    {
-	        return 0.5F;
-	    }
-	    
-	    //Darken the sky if it rains is true
-	    public boolean darkenSkyDuringRain()
-	    {
-	        return false;
-	    }
-	    
-	    //Star brightness 1.0F lets you see stars in day
-	    public float getStarBrightness(World world, float f)
-	    {
-	        return 1.0F;
-	    }
-	    
-	    //Fog Color
-	    @SideOnly(Side.CLIENT)
-	    public Vec3 getFogColor(float par1, float par2)
-	    {
-	        int var3 = 10518688;
-	        float var4 = MathHelper.cos(par1 * (float)Math.PI * 2.0F) * 2.0F + 0.5F;
 
-	        if (var4 < 0.0F)
-	        {
-	            var4 = 0.0F;
-	        }
+	public int getDimensionID() 
+	{
+		return CodeLyoko.Mountain_Sector_ID;
+	}
 
-	        if (var4 > 1.0F)
-	        {
-	            var4 = 1.0F;
-	        }
+	@Override
+	public void registerWorldChunkManager()
+	{
+		worldChunkMgr = new WorldChunkManagerHell(CodeLyoko.lyokomountain, 0.8F, 0F);
+	}
 
-	        float var5 = (float)(var3 >> 16 & 255) / 255.0F;
-	        float var6 = (float)(var3 >> 8 & 255) / 255.0F;
-	        float var7 = (float)(var3 & 255) / 255.0F;
-	        var5 *= var4 * 0.0F + 0.15F;
-	        var6 *= var4 * 0.0F + 0.15F;
-	        var7 *= var4 * 0.0F + 0.15F;
-	        return this.worldObj.getWorldVec3Pool().getVecFromPool((double)var5, (double)var6, (double)var7);
-	    }
-	    
-	    //removes clouds if set to false
-	    public boolean renderClouds()
-	    {
-	        return false;
-	    }
+	public IChunkProvider getChunkProvider()
+	{
+		return new LyokoMountainChunkProvider(worldObj, worldObj.getSeed());
+	}
 
-		@Override
-		public String getDimensionName() {
-			return "Mountain Sector";
+	@Override
+	public boolean canRespawnHere()
+	{
+		return false;
+	}
+
+
+	//Ender sky if set true
+	public boolean renderEndSky()
+	{
+		return true;
+	}
+
+	public float setSunSize()
+	{
+		return 2.0F;
+	}
+
+	public float setMoonSize()
+	{
+		return 0.5F;
+	}
+
+	//Darken the sky if it rains is true
+	public boolean darkenSkyDuringRain()
+	{
+		return false;
+	}
+
+	//Star brightness 1.0F lets you see stars in day
+	public float getStarBrightness(World world, float f)
+	{
+		return 1.0F;
+	}
+
+	//Fog Color
+	@Override
+	@SideOnly(Side.CLIENT)
+	public Vec3 getFogColor(float par1, float par2)
+	{
+		int var3 = 10518688;
+		float var4 = MathHelper.cos(par1 * (float)Math.PI * 2.0F) * 2.0F + 0.5F;
+
+		if (var4 < 0.0F)
+		{
+			var4 = 0.0F;
 		}
+
+		if (var4 > 1.0F)
+		{
+			var4 = 1.0F;
+		}
+
+		float var5 = (float)(var3 >> 16 & 255) / 255.0F;
+		float var6 = (float)(var3 >> 8 & 255) / 255.0F;
+		float var7 = (float)(var3 & 255) / 255.0F;
+		var5 *= var4 * 0.0F + 0.15F;
+		var6 *= var4 * 0.0F + 0.15F;
+		var7 *= var4 * 0.0F + 0.15F;
+		return this.worldObj.getWorldVec3Pool().getVecFromPool((double)var5, (double)var6, (double)var7);
+	}
+
+	//removes clouds if set to false
+	public boolean renderClouds()
+	{
+		return false;
+	}
+
+	@Override
+	public String getDimensionName() {
+		return "Mountain Sector";
+	}
 
 }
