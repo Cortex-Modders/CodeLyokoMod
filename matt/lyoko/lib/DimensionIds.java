c0232390f976c6b53c248ecd2b5d6ccf64b86f4d
Jake Rhoda <jake@jadarstudios.com>
2013-04-21 19:01:31 -0500

    added comments

--- matt/lyoko/lib/DimensionIds.java
+++ matt/lyoko/lib/DimensionIds.java
@@ -2,6 +2,7 @@ package matt.lyoko.lib;
 
 public class DimensionIds {
 
+    /* Default IDs. */
     public static int ICE_DEFAULT = 3;
     public static int MOUNTAIN_DEFAULT = 4;
     public static int FOREST_DEFAULT = 5;
@@ -10,6 +11,7 @@ public class DimensionIds {
     public static int CARTHAGE_DEFAULT = 8;
     public static int CORTEX_DEFAULT = 9;
     
+    /* IDs loaded from config. */
     public static int ICE;
     public static int MOUNTAIN;
     public static int FOREST;
