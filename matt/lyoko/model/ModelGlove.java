f02094e70796a2894f934c0081068c7cf5c6cbee
Jake Rhoda <jake@jadarstudios.com>
2013-04-25 16:33:36 -0500

    Work on model and fingers. one bends correctly but the others dont yet.

--- matt/lyoko/model/ModelGlove.java
+++ matt/lyoko/model/ModelGlove.java
@@ -98,26 +98,33 @@ public class ModelGlove extends ModelBase {
         finger2tip.mirror = true;
         setRotation(finger2tip, 0.418879F, 0F, 0F);
        
+        finger2.addChild(finger2mid);
+        finger2mid.addChild(finger2tip);
+        
+        
         finger3 = new ModelRenderer(this, 0, 4);
         finger3.addBox(-0.5F, 0F, -2F, 1, 1, 2);
         finger3.setRotationPoint(-1.5F, 1.5F, -3F);
         finger3.setTextureSize(64, 32);
         finger3.mirror = true;
         setRotation(finger3, 0.1570796F, 0F, 0F);
+        
         finger3mid = new ModelRenderer(this, 0, 2);
-        finger3mid.addBox(-0.5F, -0.5F, -1.5F, 1, 1, 1);
-        finger3mid.setRotationPoint(-1.5F, 2.2F, -4.4F);
+        finger3mid.addBox(0.0F, -0.7F, -1.5F, 1, 1, 1);
+        finger3mid.setRotationPoint(-0.5F, 0.545F, -1.4F);
         finger3mid.setTextureSize(64, 32);
         finger3mid.mirror = true;
-        finger3.addChild(finger3mid);
         setRotation(finger3mid, 0.2478368F, 0F, 0F);
+        
         finger3tip = new ModelRenderer(this, 0, 0);
         finger3tip.addBox(-0.5F, -0.5F, -1F, 1, 1, 1);
-        finger3tip.setRotationPoint(-1.5F, 2.5F, -5.6F);
+        finger3tip.setRotationPoint(0.5F, -0.25F, -1.4F);
         finger3tip.setTextureSize(64, 32);
         finger3tip.mirror = true;
-        finger3mid.addChild(finger3tip);
         setRotation(finger3tip, 0.418879F, 0F, 0F);
+        
+        finger3.addChild(finger3mid);
+        finger3mid.addChild(finger3tip);
     }
 
     public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5, boolean isFirstPerson) {
@@ -135,8 +142,6 @@ public class ModelGlove extends ModelBase {
         finger2mid.render(f5);
         finger2tip.render(f5);
         finger3.render(f5);
-//        finger3mid.render(f5);
-//        finger3tip.render(f5);
         if(isFirstPerson) {
             // left thumb
         }
@@ -165,9 +170,9 @@ public class ModelGlove extends ModelBase {
         finger2mid.rotateAngleX = 0.6667158F;
         finger2tip.rotateAngleX = 1.0855948F;
         
-        finger3.rotateAngleX = 0.418879F;
-        finger3mid.rotateAngleX = 0.6667158F;
-        finger3tip.rotateAngleX = 1.0855948F;
+        finger3.rotateAngleX = (float)Math.toRadians(30);
+        finger3mid.rotateAngleX = (float)Math.toRadians(30);
+        finger3tip.rotateAngleX = (float)Math.toRadians(30);
     }
     
     public void unclenchHand() {
@@ -181,6 +186,6 @@ public class ModelGlove extends ModelBase {
         
         finger3.rotateAngleX = 0.1570796F;
         finger3mid.rotateAngleX = 0.2478368F;
-        finger3tip.rotateAngleX = 0.418879F;
+//        finger3tip.rotateAngleX = 0.418879F;
     }
 }
