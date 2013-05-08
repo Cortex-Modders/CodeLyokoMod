3cba9924c4d63047ebf28cfdc7d78f52880e68f8
Matthew Warren <tonkamatt98@gmail.com>
2013-05-08 17:54:29 -0500

    added method for rendering nav skids

--- matt/lyoko/render/vehicles/RenderSkid.java
+++ matt/lyoko/render/vehicles/RenderSkid.java
@@ -1,9 +1,34 @@
 package matt.lyoko.render.vehicles;
 
+import org.lwjgl.opengl.GL11;
+
+import matt.lyoko.entities.vehicles.EntitySkid;
 import matt.lyoko.model.vehicles.ModelSkid;
 
-public class RenderSkid extends RenderVehicle {
-	public RenderSkid() {
+public class RenderSkid extends RenderVehicle
+{
+	public RenderSkid()
+	{
 		super(new ModelSkid(), 0.5F);
+	}
+	
+	public void renderNavSkids(EntitySkid entSkid)
+	{
+		if(entSkid.getNavSkid(0))
+		{
+			//TODO render front navSkid
+		}
+		if(entSkid.getNavSkid(1))
+		{
+			//TODO render back navSkid
+		}
+		if(entSkid.getNavSkid(2))
+		{
+			//TODO render left navSkid
+		}
+		if(entSkid.getNavSkid(3))
+		{
+			//TODO render right navSkid
+		}
 	}
 }
