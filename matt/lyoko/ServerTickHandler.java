331a03c3236ce8c5d8edb78e80e4afedb96cbc1a
Matthew Warren <tonkamatt98@gmail.com>
2013-05-11 02:05:03 -0500

    fixed bug that broke servers

--- matt/lyoko/ServerTickHandler.java
+++ matt/lyoko/ServerTickHandler.java
@@ -12,45 +12,33 @@ import cpw.mods.fml.common.TickType;
 
 public class ServerTickHandler implements ITickHandler
 {
-	private void onPlayerTick(EntityPlayer entp)
-    {
-		Minecraft mc = Minecraft.getMinecraft();
-		if((mc.theWorld != null) && (mc.theWorld.playerEntities.size() > 0) && CodeLyoko.enableAdminPowers)
+	private void onPlayerTick(EntityPlayer player)
+	{
+		if(player.username.equals(CodeLyoko.getDevelopers()[0]))
 		{
-            List<EntityPlayer> players = mc.theWorld.playerEntities;
-            for (int counter = 0; counter < players.size(); counter++)
-            {
-                if (players.get(counter) != null)
-                {
-                    EntityPlayer player = players.get(counter);
-                    if(player.username.equals(CodeLyoko.getDevelopers()[0]))
-                    {
-                    	player.capabilities.allowFlying = true;
-        				player.fallDistance = 0;
-                    	player.sendPlayerAbilities();
-                    }
-                    else if(player.username.equals(CodeLyoko.getDevelopers()[1]))
-                    {
-                    	player.addPotionEffect((new PotionEffect(Potion.jump.getId(), 20, 3)));
-        				player.fallDistance = 0;
-                    }
-                    else if(player.username.equals(CodeLyoko.getDevelopers()[2]))
-                    {
-                    	player.addPotionEffect((new PotionEffect(Potion.jump.getId(), 20, 3)));
-        				player.fallDistance = 0;
-                    }
-                    else if(player.username.equals(CodeLyoko.getDevelopers()[3]))
-                    {
-                    	if(player.isSprinting())
-        				{
-        					player.addPotionEffect((new PotionEffect(Potion.moveSpeed.getId(), 20, 2)));
-        				}
-        				player.fallDistance = 0;
-                    }
-                }
-            }
-        }
-    }
+			player.capabilities.allowFlying = true;
+			player.fallDistance = 0;
+			player.sendPlayerAbilities();
+		}
+		else if(player.username.equals(CodeLyoko.getDevelopers()[1]))
+		{
+			player.addPotionEffect((new PotionEffect(Potion.jump.getId(), 20, 3)));
+			player.fallDistance = 0;
+		}
+		else if(player.username.equals(CodeLyoko.getDevelopers()[2]))
+		{
+			player.addPotionEffect((new PotionEffect(Potion.jump.getId(), 20, 3)));
+			player.fallDistance = 0;
+		}
+		else if(player.username.equals(CodeLyoko.getDevelopers()[3]))
+		{
+			if(player.isSprinting())
+			{
+				player.addPotionEffect((new PotionEffect(Potion.moveSpeed.getId(), 20, 2)));
+			}
+			player.fallDistance = 0;
+		}
+	}
     
     @Override
     public void tickStart(EnumSet<TickType> type, Object... tickData)
