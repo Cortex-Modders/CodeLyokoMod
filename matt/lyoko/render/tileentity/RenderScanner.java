10943c0f302a31fceaf9a47a7cc0607b55891509
Matthew Warren <tonkamatt98@gmail.com>
2013-06-22 18:29:41 -0500

    Revert "more stuff on the scanner multiblock and stuff"
    This reverts commit 90f99c245a2dc20b70f76471bcbca0ad1a02aaad.

--- matt/lyoko/render/tileentity/RenderScanner.java
+++ matt/lyoko/render/tileentity/RenderScanner.java
@@ -2,7 +2,6 @@ package matt.lyoko.render.tileentity;
 
 import matt.lyoko.entities.tileentity.TileEntityScanner;
 import matt.lyoko.model.tileentity.ModelScanner;
-import matt.lyoko.model.tileentity.ModelScannerBlock;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.tileentity.TileEntity;
 
@@ -14,26 +13,21 @@ import cpw.mods.fml.relauncher.SideOnly;
 @SideOnly(Side.CLIENT)
 public class RenderScanner extends TileEntitySpecialRenderer {
 
-    private ModelScanner modelFull = new ModelScanner();
-    private ModelScannerBlock modelBlock = new ModelScannerBlock();
-    
+    private ModelScanner model = new ModelScanner();
+
     /**
-     * Renders the full model for when it is a multiblock.
-     * 
-     * @param entity
-     * @param x
-     * @param y
-     * @param z
-     * @param tick
+     * Renders the TileEntity for the chest at a position.
      */
-    public void renderFull(TileEntityScanner entity, double x, double y, double z, float tick) {
+    public void render(TileEntityScanner entity, double x, double y, double z, float tick) {
         int i;
+        System.out.println(tick);
         if (!entity.func_70309_m()) {
             i = 0;
         } else {
             i = entity.getBlockMetadata();
         }
 
+        // /mods/lyoko/textures/models/scanner.png
         this.bindTextureByName("/mods/lyoko/textures/models/scanner.png");
         GL11.glPushMatrix();
         GL11.glTranslatef((float) x, (float) y, (float) z);
@@ -61,77 +55,20 @@ public class RenderScanner extends TileEntitySpecialRenderer {
 
         GL11.glRotatef(rotate, 0F, 1F, 0F);
 
-        modelFull.doorL.rotateAngleY = (float)Math.toRadians(entity.doorRotationYaw);
-        modelFull.doorL.rotationPointX = entity.doorPosX;
-        modelFull.doorL.rotationPointZ = entity.doorPosZ;
+        model.doorL.rotateAngleY = (float)Math.toRadians(entity.doorRotationYaw);
+        model.doorL.rotationPointX = entity.doorPosX;
+        model.doorL.rotationPointZ = entity.doorPosZ;
         
-        modelFull.doorR.rotateAngleY = -(float)Math.toRadians((entity.doorRotationYaw + 180));
-        modelFull.doorR.rotationPointX = -entity.doorPosX;
-        modelFull.doorR.rotationPointZ = entity.doorPosZ;
+        model.doorR.rotateAngleY = -(float)Math.toRadians((entity.doorRotationYaw + 180));
+        model.doorR.rotationPointX = -entity.doorPosX;
+        model.doorR.rotationPointZ = entity.doorPosZ;
         
-        modelFull.render(entity, (float) x, (float) y, (float) z, 0.0F, 0.0F, 0.0625F);
+        model.render(entity, (float) x, (float) y, (float) z, 0.0F, 0.0F, 0.0625F);
         GL11.glPopMatrix();
     }
 
-    /**
-     * Renders the model of a single scanner block for when it is not a multiblock.
-     * 
-     * @param entity
-     * @param x
-     * @param y
-     * @param z
-     * @param tick
-     */
-    public void renderBlock(TileEntityScanner entity, double x, double y, double z, float tick) {
-        int i;
-        if (!entity.func_70309_m()) {
-            i = 0;
-        } else {
-            i = entity.getBlockMetadata();
-        }
-        
-        this.bindTextureByName("/mods/lyoko/textures/models/scanner.png");
-        GL11.glPushMatrix();
-        GL11.glTranslatef((float) x, (float) y, (float) z);
-        GL11.glTranslatef(0.5F, 0.5F, 0.5F);
-        // Use this or else model renders upside-down. o.O
-        GL11.glRotatef(180, 0F, 0F, 1F);
-
-        short rotate = 0;
-
-        if (i == 0) {
-            rotate = 0;
-        }
-
-        if (i == 1) {
-            rotate = 90;
-        }
-        
-        if (i == 2) {
-            rotate = 180;
-        }
-
-        if (i == 3) {
-            rotate = -90;
-        }
-
-        GL11.glRotatef(rotate, 0F, 1F, 0F);
-
-        modelBlock.render(entity, (float) x, (float) y, (float) z, 0.0F, 0.0F, 0.0625F);
-        GL11.glPopMatrix();
-    }
-    
-    
-    /**
-     * Chooses which model to render based on the isMultiblock field in TileEntityScanner.
-     */
     @Override
-    public void renderTileEntityAt(TileEntity entity, double x, double y, double z, float tick) {
-        if(!(entity instanceof TileEntityScanner)) return;
-        TileEntityScanner scanner = (TileEntityScanner) entity;
-        
-        if(scanner.isMultiblock && scanner.isCoreBlock) this.renderFull(scanner, x, y, z, tick);
-        else if(scanner.isMultiblock && !scanner.isCoreBlock) return;
-        else this.renderBlock(scanner, x, y, z, tick);
+    public void renderTileEntityAt(TileEntity entity, double par2, double par4, double par6, float par8) {
+        this.render((TileEntityScanner) entity, par2, par4, par6, par8);
     }
 }
