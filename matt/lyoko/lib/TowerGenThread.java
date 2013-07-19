32889460a20ff6fcfc9d0b3111bfb73e05fd80fa
Matthew Warren <tonkamatt98@gmail.com>
2013-07-19 19:10:55 -0400

    now using thread to generate tower.
    no change in ammount of lag yet

--- matt/lyoko/lib/TowerGenThread.java
+++ matt/lyoko/lib/TowerGenThread.java
@@ -1,12 +1,30 @@
 package matt.lyoko.lib;
 
+import java.util.Random;
+
+import net.minecraft.world.World;
 import matt.lyoko.world.StructureTower;
 
 public class TowerGenThread implements Runnable
 {
+	private World world;
+	private Random rand;
+	private int x;
+	private int y;
+	private int z;
+	
+	public TowerGenThread(World world, Random rand, int x, int y, int z)
+	{
+		this.world = world;
+		this.rand = rand;
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+	
 	@Override
 	public void run()
 	{
-		
+		(new StructureTower()).generate(world, rand, x, y, z);
 	}
 }
--- matt/lyoko/world/WorldGenTower.java
+++ matt/lyoko/world/WorldGenTower.java
@@ -3,6 +3,7 @@ package matt.lyoko.world;
 import java.util.Random;
 
 import matt.lyoko.lib.DimensionIds;
+import matt.lyoko.lib.TowerGenThread;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.IChunkProvider;
 import cpw.mods.fml.common.IWorldGenerator;
@@ -33,6 +34,7 @@ public class WorldGenTower implements IWorldGenerator
 		  int Ycoord1 = random.nextInt(128);
 		  int Zcoord1 = blockZ + random.nextInt(16);
 		  
-		  (new StructureTower()).generate(world, random, Xcoord1, Ycoord1, Zcoord1); 
+		  //(new StructureTower()).generate(world, random, Xcoord1, Ycoord1, Zcoord1);
+		  (new Thread(new TowerGenThread(world, random, Xcoord1, Ycoord1, Zcoord1))).start();
 	 }
 }
