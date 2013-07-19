bb201cea658e06d9cd5ac4a30df4e6d22d8b51e6
Matthew Warren <tonkamatt98@gmail.com>
2013-07-19 01:54:29 -0400

    tower is 95% complete. (basically complete)
    also modified multi-block conditions for tower floor.

--- matt/lyoko/items/ItemLyoko.java
+++ matt/lyoko/items/ItemLyoko.java
@@ -3,6 +3,9 @@ package matt.lyoko.items;
 import java.util.List;
 
 import matt.lyoko.CodeLyoko;
+import matt.lyoko.entities.vehicles.EntityOverboard;
+import matt.lyoko.world.StructureTower;
+import net.minecraft.block.Block;
 import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLiving;
@@ -11,6 +14,11 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.util.EnumMovingObjectType;
+import net.minecraft.util.MathHelper;
+import net.minecraft.util.MovingObjectPosition;
+import net.minecraft.util.Vec3;
 import net.minecraft.world.World;
 
 public class ItemLyoko extends Item
@@ -34,20 +42,99 @@ public class ItemLyoko extends Item
 	@Override
     public ItemStack onItemRightClick(ItemStack stack, World world, EntityPlayer player)
     {
-    	if(player.isSneaking() && stack.getItem().equals(ModItems.LaserArrow))
-    	{
-    		Item item;
-    		int index = CodeLyoko.debugTools.indexOf(this);
-    		if(CodeLyoko.debugTools.size() == index + 1)
-    		{
-    			item = CodeLyoko.debugTools.get(0);
-    		}
-    		else
-    		{
-    			item = CodeLyoko.debugTools.get(index + 1);
-    		}
-    		stack = new ItemStack(item, stack.stackSize);
-    	}
+		if(stack.getItem().equals(ModItems.LaserArrow))
+		{
+			if(player.isSneaking())
+			{
+				Item item;
+				int index = CodeLyoko.debugTools.indexOf(this);
+				if(CodeLyoko.debugTools.size() == index + 1)
+				{
+					item = CodeLyoko.debugTools.get(0);
+				}
+				else
+				{
+					item = CodeLyoko.debugTools.get(index + 1);
+				}
+				stack = new ItemStack(item, stack.stackSize);
+			}
+			else
+			{
+				float f = 1.0F;
+		        float f1 = player.prevRotationPitch + (player.rotationPitch - player.prevRotationPitch) * f;
+		        float f2 = player.prevRotationYaw + (player.rotationYaw - player.prevRotationYaw) * f;
+		        double d0 = player.prevPosX + (player.posX - player.prevPosX) * (double)f;
+		        double d1 = player.prevPosY + (player.posY - player.prevPosY) * (double)f + 1.62D - (double)player.yOffset;
+		        double d2 = player.prevPosZ + (player.posZ - player.prevPosZ) * (double)f;
+		        Vec3 vec3 = world.getWorldVec3Pool().getVecFromPool(d0, d1, d2);
+		        float f3 = MathHelper.cos(-f2 * 0.017453292F - (float)Math.PI);
+		        float f4 = MathHelper.sin(-f2 * 0.017453292F - (float)Math.PI);
+		        float f5 = -MathHelper.cos(-f1 * 0.017453292F);
+		        float f6 = MathHelper.sin(-f1 * 0.017453292F);
+		        float f7 = f4 * f5;
+		        float f8 = f3 * f5;
+		        double d3 = 5.0D;
+		        Vec3 vec31 = vec3.addVector((double)f7 * d3, (double)f6 * d3, (double)f8 * d3);
+		        MovingObjectPosition movingobjectposition = world.clip(vec3, vec31, true);
+		        
+		        if (movingobjectposition == null)
+		        {
+		            return stack;
+		        }
+		        else
+		        {
+		            Vec3 vec32 = player.getLook(f);
+		            boolean flag = false;
+		            float f9 = 1.0F;
+		            List list = world.getEntitiesWithinAABBExcludingEntity(player, player.boundingBox.addCoord(vec32.xCoord * d3, vec32.yCoord * d3, vec32.zCoord * d3).expand((double)f9, (double)f9, (double)f9));
+		            int i;
+		            
+		            for (i = 0; i < list.size(); ++i)
+		            {
+		                Entity entity = (Entity)list.get(i);
+		                
+		                if (entity.canBeCollidedWith())
+		                {
+		                    float f10 = entity.getCollisionBorderSize();
+		                    AxisAlignedBB axisalignedbb = entity.boundingBox.expand((double)f10, (double)f10, (double)f10);
+		                    
+		                    if (axisalignedbb.isVecInside(vec3))
+		                    {
+		                        flag = true;
+		                    }
+		                }
+		            }
+		            
+		            if (flag)
+		            {
+		                return stack;
+		            }
+		            else
+		            {
+		                if (movingobjectposition.typeOfHit == EnumMovingObjectType.TILE)
+		                {
+		                    i = movingobjectposition.blockX;
+		                    int j = movingobjectposition.blockY;
+		                    int k = movingobjectposition.blockZ;
+		                    
+		                    if (world.getBlockId(i, j, k) == Block.snow.blockID)
+		                    {
+		                        --j;
+		                    }
+		                    
+		                    StructureTower st = new StructureTower();
+		                    st.generate(world, itemRand, i - 3, j, k - 3);
+		                    
+		                    if (!player.capabilities.isCreativeMode)
+		                    {
+		                        --stack.stackSize;
+		                    }
+		                }
+		                return stack;
+		            }
+		        }
+			}
+		}
     	return stack;
     }
 	
