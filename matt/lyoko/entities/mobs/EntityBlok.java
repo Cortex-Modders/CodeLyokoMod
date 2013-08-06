3b9174a05d95904df40e8a0e31e0543ad4995c33
Jadar <jake@jadarstudios.com>
2013-08-05 22:38:18 -0500

    changed license to MIT license. YOU MUST INCLUDE OUR COPYRIGHT TO USE OUR CODE!

--- matt/lyoko/entities/mobs/EntityBlok.java
+++ matt/lyoko/entities/mobs/EntityBlok.java
@@ -1,3 +1,13 @@
+/*
+ * Code Lyoko Mod for Minecraft v@VERSION
+ *
+ * Copyright 2013 Cortex Modders, Matthew Warren, Jacob Rhoda, and
+ * other contributors.
+ * Released under the MIT license
+ * http://opensource.org/licenses/MIT
+ * 
+ */
+
 package matt.lyoko.entities.mobs;
 
 import cpw.mods.fml.relauncher.Side;
--- matt/lyoko/render/tileentity/RenderSuperCalcConsole.java
+++ matt/lyoko/render/tileentity/RenderSuperCalcConsole.java
@@ -1,3 +1,13 @@
+/*
+ * Code Lyoko Mod for Minecraft v@VERSION
+ *
+ * Copyright 2013 Cortex Modders, Matthew Warren, Jacob Rhoda, and
+ * other contributors.
+ * Released under the MIT license
+ * http://opensource.org/licenses/MIT
+ * 
+ */
+
 package matt.lyoko.render.tileentity;
 
 import matt.lyoko.entities.tileentity.TileEntitySuperCalcConsole;
